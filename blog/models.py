a0d8d61a24a39bd4971767b4ea4c035396893eb4
hyejin <wls5258@jbnu.ac.kr>
2022-11-14 20:39:34 +0900

    장고 화잇팅

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 from django.contrib.auth.models import User
 
 
@@ -12,9 +13,14 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categroies'
 
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -22,9 +28,16 @@ class Post(models.Model):
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     categroy = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.get_file_name().split(',')[-1]
+
+
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
-
